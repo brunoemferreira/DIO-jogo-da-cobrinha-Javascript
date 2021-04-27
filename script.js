c983dfdef07c52a2b63db10962e7bd19886d0201
brunoemferreira <brunoemf@gmail.com>
2021-04-27 13:01:14 -0300

    creating snake

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+};
 
 function criarBG() {
   // Define a cor
@@ -9,4 +14,13 @@ function criarBG() {
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha() {
+  for (i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
 criarBG();
+criarCobrinha();
+// A cobrinha é um array de coordenadas onde adiciona 1 e retira o ultimo
