4f24ff15c3b2c6a630b0c5d69a900db511af72ae
brunoemferreira <brunoemf@gmail.com>
2021-04-27 13:12:35 -0300

    creating snake moviment

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/* Observações 
+  - A cobrinha é um array de coordenadas onde adiciona 1 e retira o ultimo
+
+
+*/
+
+
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
@@ -6,6 +13,7 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box
 };
+let direction = "right";
 
 function criarBG() {
   // Define a cor
@@ -21,6 +29,29 @@ function criarCobrinha() {
   }
 }
 
-criarBG();
-criarCobrinha();
-// A cobrinha é um array de coordenadas onde adiciona 1 e retira o ultimo
+function iniciarJogo() {
+  criarBG();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+
+  // Retira a ultima posição do array ou seja da cobrinha 
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  };
+
+  snake.unshift(newHead);
+
+};
+
+let jogo = setInterval(iniciarJogo, 100);
+
