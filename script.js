d1b2a96dfed59f8d98ab89b453e774906384ff57
brunoemferreira <brunoemf@gmail.com>
2021-04-27 13:36:15 -0300

    defining directions and preventing the snake from disappearing

--- script.js
+++ script.js
@@ -29,7 +29,22 @@ function criarCobrinha() {
   }
 }
 
+// Captura o evento de keydown e executa a função update
+document.addEventListener('keydown', update);
+
+function update() {
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+};
+
 function iniciarJogo() {
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
   criarBG();
   criarCobrinha();
 
@@ -53,5 +68,5 @@ function iniciarJogo() {
 
 };
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 200);
 
